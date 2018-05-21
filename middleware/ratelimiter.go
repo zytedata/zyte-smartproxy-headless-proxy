5b2d171c6f50b4bb0fd9c493cec7a668ecf966aa
9seconds <nineseconds@yandex.ru>
2018-05-21 19:20:39 +0300

    Do not use semaphore library

--- middleware/ratelimiter.go
+++ middleware/ratelimiter.go
@@ -4,8 +4,6 @@ import (
 	"net/http"
 
 	"github.com/elazarl/goproxy"
-	"github.com/kamilsk/semaphore"
-	log "github.com/sirupsen/logrus"
 
 	"github.com/9seconds/crawlera-headless-proxy/config"
 )
@@ -13,33 +11,19 @@ import (
 type rateLimiterMiddleware struct {
 	UniqBase
 
-	limiter semaphore.Semaphore
+	limiter chan struct{}
 }
 
 func (rl *rateLimiterMiddleware) OnRequest() ReqType {
 	return rl.BaseOnRequest(func(req *http.Request, ctx *goproxy.ProxyCtx) (*http.Request, *http.Response) {
-		if _, err := rl.limiter.Acquire(nil); err != nil {
-			rstate := GetRequestState(ctx)
-			log.WithFields(log.Fields{
-				"request-id": rstate.ID,
-				"client-id":  rstate.ClientID,
-				"error":      err,
-			}).Warn("Error on acquiring semaphore.")
-		}
+		rl.limiter <- struct{}{}
 		return req, nil
 	})
 }
 
 func (rl *rateLimiterMiddleware) OnResponse() RespType {
 	return rl.BaseOnResponse(func(resp *http.Response, ctx *goproxy.ProxyCtx) *http.Response {
-		if err := rl.limiter.Release(); err != nil {
-			rstate := GetRequestState(ctx)
-			log.WithFields(log.Fields{
-				"request-id": rstate.ID,
-				"client-id":  rstate.ClientID,
-				"error":      err,
-			}).Warn("Error on releasing semaphore.")
-		}
+		<-rl.limiter
 		return resp
 	})
 }
@@ -50,7 +34,7 @@ func NewRateLimiterMiddleware(conf *config.Config, proxy *goproxy.ProxyHttpServe
 	ware := &rateLimiterMiddleware{}
 	ware.mtype = middlewareTypeRateLimiter
 
-	ware.limiter = semaphore.New(conf.ConcurrentConnections)
+	ware.limiter = make(chan struct{}, conf.ConcurrentConnections)
 
 	return ware
 }
