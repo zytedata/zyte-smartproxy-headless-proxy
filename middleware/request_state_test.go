36b446b79ca2ed053fbac427d1f8f34522a987b7
9seconds <nineseconds@yandex.ru>
2018-06-04 16:29:26 +0300

    Refactor tests for request state

--- middleware/request_state_test.go
+++ middleware/request_state_test.go
@@ -5,96 +5,86 @@ import (
 	"testing"
 	"time"
 
-	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/suite"
 	gock "gopkg.in/h2non/gock.v1"
 )
 
-func TestRequestStateFinish(t *testing.T) {
-	cr := testNewProxyContainer()
-	state, err := newRequestState(cr.req, cr.s)
-	assert.Nil(t, err)
+type RequestStateSuite struct {
+	suite.Suite
 
-	state.Finish()
-	assert.WithinDuration(t, state.requestFinished, time.Now(), time.Second)
-
-	oldFinish := state.requestFinished
-	state.Finish()
-	assert.Equal(t, oldFinish, state.requestFinished)
+	cr    *testProxyContainer
+	state *RequestState
 }
 
-func TestRequestStateElapsed(t *testing.T) {
-	cr := testNewProxyContainer()
-	state, err := newRequestState(cr.req, cr.s)
-	assert.Nil(t, err)
+func (t *RequestStateSuite) SetupTest() {
+	t.cr = testNewProxyContainer()
+	state, _ := newRequestState(t.cr.req, t.cr.s)
+	t.state = state
+}
 
-	assert.InEpsilon(t, state.Elapsed(), time.Millisecond, float64(time.Millisecond))
-	state.Finish()
+func (t *RequestStateSuite) TestFinish() {
+	t.state.Finish()
+	t.WithinDuration(t.state.requestFinished, time.Now(), time.Second)
+
+	oldFinish := t.state.requestFinished
+	t.state.Finish()
+	t.Equal(oldFinish, t.state.requestFinished)
+}
+
+func (t *RequestStateSuite) TestElapsed() {
+	t.InEpsilon(t.state.Elapsed(), time.Millisecond, float64(time.Millisecond))
+	t.state.Finish()
+
 	time.Sleep(500 * time.Millisecond)
-	assert.InEpsilon(t, state.Elapsed(), 500*time.Millisecond, float64(time.Millisecond))
+	t.InEpsilon(t.state.Elapsed(), 500*time.Millisecond, float64(time.Millisecond))
 }
 
-func TestRequestStateStartCrawleraRequest(t *testing.T) {
-	cr := testNewProxyContainer()
-	state, err := newRequestState(cr.req, cr.s)
-	assert.Nil(t, err)
+func (t *RequestStateSuite) TestStartCrawleraRequest() {
+	t.Nil(t.state.StartCrawleraRequest())
+	t.Equal(t.state.CrawleraRequests, uint8(1))
+	t.Len(t.state.crawleraTimes, 1)
+	t.WithinDuration(t.state.crawleraTimes[0], time.Now(), time.Millisecond)
+	t.Len(t.state.crawleraRequestsChan, 1)
 
-	assert.Nil(t, state.StartCrawleraRequest())
-	assert.Equal(t, state.CrawleraRequests, uint8(1))
-	assert.Len(t, state.crawleraTimes, 1)
-	assert.WithinDuration(t, state.crawleraTimes[0], time.Now(), time.Millisecond)
-	assert.Len(t, state.crawleraRequestsChan, 1)
-
-	assert.NotNil(t, state.StartCrawleraRequest())
-	assert.Len(t, state.crawleraRequestsChan, 1)
-	assert.Len(t, state.crawleraTimesChan, 0)
-	assert.Equal(t, state.CrawleraRequests, uint8(1))
-	assert.Len(t, state.crawleraTimes, 1)
+	t.NotNil(t.state.StartCrawleraRequest())
+	t.Len(t.state.crawleraRequestsChan, 1)
+	t.Len(t.state.crawleraTimesChan, 0)
+	t.Equal(t.state.CrawleraRequests, uint8(1))
+	t.Len(t.state.crawleraTimes, 1)
 }
 
-func TestRequestStateFinishCrawleraRequest(t *testing.T) {
-	cr := testNewProxyContainer()
-	state, err := newRequestState(cr.req, cr.s)
-	assert.Nil(t, err)
+func (t *RequestStateSuite) TestFinishCrawleraRequest() {
+	t.NotNil(t.state.FinishCrawleraRequest())
+	t.Len(t.state.crawleraRequestsChan, 0)
+	t.Len(t.state.crawleraTimesChan, 0)
+	t.Equal(t.state.CrawleraRequests, uint8(0))
 
-	assert.NotNil(t, state.FinishCrawleraRequest())
-	assert.Len(t, state.crawleraRequestsChan, 0)
-	assert.Len(t, state.crawleraTimesChan, 0)
-	assert.Equal(t, state.CrawleraRequests, uint8(0))
+	t.state.StartCrawleraRequest()
+	t.Nil(t.state.FinishCrawleraRequest())
+	t.Len(t.state.crawleraRequestsChan, 1)
+	t.Len(t.state.crawleraTimesChan, 1)
+	t.Equal(t.state.CrawleraRequests, uint8(1))
+	t.Len(t.state.crawleraTimes, 2)
 
-	state.StartCrawleraRequest()
-	assert.Nil(t, state.FinishCrawleraRequest())
-	assert.Len(t, state.crawleraRequestsChan, 1)
-	assert.Len(t, state.crawleraTimesChan, 1)
-	assert.Equal(t, state.CrawleraRequests, uint8(1))
-	assert.Len(t, state.crawleraTimes, 2)
-
-	assert.WithinDuration(t, state.crawleraTimes[0], time.Now(), time.Millisecond)
-	assert.NotNil(t, state.FinishCrawleraRequest())
-	assert.Len(t, state.crawleraRequestsChan, 1)
-	assert.Len(t, state.crawleraTimesChan, 1)
-	assert.Equal(t, state.CrawleraRequests, uint8(1))
-	assert.Len(t, state.crawleraTimes, 2)
+	t.WithinDuration(t.state.crawleraTimes[0], time.Now(), time.Millisecond)
+	t.NotNil(t.state.FinishCrawleraRequest())
+	t.Len(t.state.crawleraRequestsChan, 1)
+	t.Len(t.state.crawleraTimesChan, 1)
+	t.Equal(t.state.CrawleraRequests, uint8(1))
+	t.Len(t.state.crawleraTimes, 2)
 }
 
-func TestRequestStateCrawleraElapsed(t *testing.T) {
-	cr := testNewProxyContainer()
-	state, err := newRequestState(cr.req, cr.s)
-	assert.Nil(t, err)
-
-	assert.Zero(t, state.CrawleraElapsed())
-	state.StartCrawleraRequest()
-	assert.NotZero(t, state.CrawleraElapsed())
+func (t *RequestStateSuite) TestCrawleraElapsed() {
+	t.Zero(t.state.CrawleraElapsed())
+	t.state.StartCrawleraRequest()
+	t.NotZero(t.state.CrawleraElapsed())
 	time.Sleep(500 * time.Millisecond)
 
-	state.FinishCrawleraRequest()
-	assert.InEpsilon(t, state.CrawleraElapsed(), 500*time.Millisecond, float64(time.Millisecond))
+	t.state.FinishCrawleraRequest()
+	t.InEpsilon(t.state.CrawleraElapsed(), 500*time.Millisecond, float64(time.Millisecond))
 }
 
-func TestRequestStateDoCrawleraRequest(t *testing.T) {
-	cr := testNewProxyContainer()
-	state, err := newRequestState(cr.req, cr.s)
-	assert.Nil(t, err)
-
+func (t *RequestStateSuite) TestDoCrawleraRequest() {
 	newClient := &http.Client{}
 	defer gock.Off()
 	defer gock.RestoreClient(newClient)
@@ -104,9 +94,13 @@ func TestRequestStateDoCrawleraRequest(t *testing.T) {
 		Reply(200).
 		BodyString("")
 
-	state.DoCrawleraRequest(newClient, cr.req)
-	assert.Len(t, state.crawleraRequestsChan, 1)
-	assert.Len(t, state.crawleraTimesChan, 1)
-	assert.Equal(t, state.CrawleraRequests, uint8(1))
-	assert.Len(t, state.crawleraTimes, 2)
+	t.state.DoCrawleraRequest(newClient, t.cr.req)
+	t.Len(t.state.crawleraRequestsChan, 1)
+	t.Len(t.state.crawleraTimesChan, 1)
+	t.Equal(t.state.CrawleraRequests, uint8(1))
+	t.Len(t.state.crawleraTimes, 2)
+}
+
+func TestRequestState(t *testing.T) {
+	suite.Run(t, &RequestStateSuite{})
 }
