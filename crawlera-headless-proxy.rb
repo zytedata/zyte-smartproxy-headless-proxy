0822c7d3cf1e3ab1543fca852390a4f0c3360ca1
9seconds <nineseconds@yandex.ru>
2019-07-02 11:51:38 +0300

    Update version in homebrew file

--- crawlera-headless-proxy.rb
+++ crawlera-headless-proxy.rb
@@ -22,7 +22,7 @@ class CrawleraHeadlessProxy < Formula
   homepage "https://github.com/scrapinghub/crawlera-headless-proxy"
 
   revision 0
-  url "https://github.com/scrapinghub/crawlera-headless-proxy.git", :using => :git, :tag => "1.1.1"
+  url "https://github.com/scrapinghub/crawlera-headless-proxy.git", :using => :git, :tag => "1.2.0"
   head "https://github.com/scrapinghub/crawlera-headless-proxy.git"
 
   depends_on "go" => :build
